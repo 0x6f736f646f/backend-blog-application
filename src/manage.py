cc12b938008f5db0e71bbc5bdbcf9fa2bac1dc9d
0x6f736f646f <blackd0t@protonmail.com>
2020-04-20 23:51:35 +0300

    [BUG] adding return 0 instead or result 0 at test

--- src/manage.py
+++ src/manage.py
@@ -29,7 +29,7 @@ def test():
     tests = unittest.TestLoader().discover('tests', pattern="test*.py")
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
-        result 0
+        return 0
     return 1
 
 
